f46dcc2611dd9edd14683de92bee53e8366f3202
Scorp-King <128297457+Tushar-Nandy@users.noreply.github.com>
2026-07-10 22:45:30 +0530

    Update subject colors in subjects.js

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -1,167 +1,84 @@
 const subjects = [
-
   {
-
     id: 1,
-
     name: "English",
-
-    color: "#3B82F6", // Bright Blue
-
+    color: "#60A5FA", // Blue
   },
-
   {
-
     id: 2,
-
     name: "Hindi",
-
-    color: "#FF6B35", // Orange
-
+    color: "#FB923C", // Orange
   },
-
   {
-
     id: 3,
-
     name: "Marathi",
-
-    color: "#F4A261", // Peach
-
+    color: "#FDBA74", // Peach
   },
-
   {
-
     id: 4,
-
     name: "Maths",
-
-    color: "#5E60CE", // Indigo
-
+    color: "#818CF8", // Indigo
   },
-
   {
-
     id: 5,
-
     name: "Science",
-
-    color: "#00C897", // Emerald
-
+    color: "#4ADE80", // Green
   },
-
   {
-
     id: 6,
-
     name: "Social Studies",
-
-    color: "#FFD166", // Golden Yellow
-
+    color: "#FACC15", // Yellow
   },
-
   {
-
     id: 7,
-
     name: "Biology",
-
-    color: "#FF4FA3", // Bio Pink
-
+    color: "#F472B6", // 🌸 Pink (Client Request)
   },
-
   {
-
     id: 8,
-
     name: "Physics",
-
-    color: "#00B4D8", // Cyan Blue
-
+    color: "#38BDF8", // Cyan
   },
-
   {
-
     id: 9,
-
     name: "Chemistry",
-
-    color: "#C77DFF", // Lavender
-
+    color: "#C084FC", // Lavender
   },
-
   {
-
     id: 10,
-
     name: "History",
-
-    color: "#A97142", // Coffee Brown
-
+    color: "#D4A373", // Brown
   },
-
   {
-
     id: 11,
-
     name: "Political Science",
-
-    color: "#E63946", // Crimson Red
-
+    color: "#F87171", // Salmon
   },
-
   {
-
     id: 12,
-
     name: "Economics",
-
-    color: "#F72585", // Magenta
-
+    color: "#F9A03F", // Amber
   },
-
   {
-
     id: 13,
-
     name: "Geography",
-
-    color: "#4CC9F0", // Sky Blue
-
+    color: "#2DD4BF", // Turquoise
   },
-
   {
-
     id: 14,
-
     name: "Maths 1 | Science",
-
-    color: "#9D4EDD", // Royal Purple
-
+    color: "#A3E635", // Lime
   },
-
   {
-
     id: 15,
-
     name: "Maths 2 | Science",
-
-    color: "#8338EC", // Deep Violet
-
+    color: "#F9A8D4", // Rose
   },
-
   {
-
     id: 16,
-
     name: "Test",
-
-    color: "#EF233C", // Bright Red
-
+    color: "#EF4444", // Bright Red
   }
-
 ];
-
-
 
 export default subjects;
